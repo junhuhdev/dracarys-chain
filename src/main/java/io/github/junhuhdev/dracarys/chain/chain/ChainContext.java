d9a3a456c6a8376a7107aa272a6900ad9970439b
Jun Huh <junhuhdev@gmail.com>
2021-01-09 15:17:00 +0100

    Update ChainContext.java

--- src/main/java/io/github/junhuhdev/dracarys/chain/chain/ChainContext.java
+++ src/main/java/io/github/junhuhdev/dracarys/chain/chain/ChainContext.java
@@ -12,70 +12,69 @@ import static java.util.Objects.requireNonNull;
 
 public class ChainContext {
 
-	private final EventTransaction xEventTransaction;
+    private final EventTransaction xEventTransaction;
 
-	public ChainContext(EventTransaction xEventTransaction) {
-		this.xEventTransaction = xEventTransaction;
-	}
+    public ChainContext(EventTransaction xEventTransaction) {
+        this.xEventTransaction = xEventTransaction;
+    }
 
-	public EventTransaction getEventTransaction() {
-		return xEventTransaction;
-	}
+    public EventTransaction getEventTransaction() {
+        return xEventTransaction;
+    }
 
-	public void setNextWorkflow(String workflow) {
-		xEventTransaction.setWorkflow(workflow);
-	}
+    public void setNextWorkflow(String workflow) {
+        xEventTransaction.setWorkflow(workflow);
+    }
 
-	public String getWorkflow() {
-		return xEventTransaction.getWorkflow();
-	}
+    public String getWorkflow() {
+        return xEventTransaction.getWorkflow();
+    }
 
-	public Long getId() {
-		return xEventTransaction.getId();
-	}
+    public Long getId() {
+        return xEventTransaction.getId();
+    }
 
-	public Long getParentId() {
-		return xEventTransaction.getParentId();
-	}
+    public Long getParentId() {
+        return xEventTransaction.getParentId();
+    }
 
-	public Long getOriginId() {
-		return isNull(xEventTransaction.getParentId()) ? xEventTransaction.getId() : xEventTransaction.getParentId();
-	}
+    public Long getOriginId() {
+        return isNull(xEventTransaction.getParentId()) ? xEventTransaction.getId() : xEventTransaction.getParentId();
+    }
 
-	public List<Event> getEvents() {
-		return xEventTransaction.getEvents();
-	}
+    public List<Event> getEvents() {
+        return xEventTransaction.getEvents();
+    }
 
-	public EventState getState() {
-		return xEventTransaction.getState();
-	}
+    public EventState getState() {
+        return xEventTransaction.getState();
+    }
 
-	public <T extends Event> T getFirstEvent(Class<T> event) {
-		return this.xEventTransaction.getFirstEvent(event);
-	}
+    public <T extends Event> T getFirstEvent(Class<T> event) {
+        return this.xEventTransaction.getFirstEvent(event);
+    }
 
-	public <T extends Event> T getLatestEvent(Class<T> event) {
-		return this.xEventTransaction.getLatestEvent(event);
-	}
+    public <T extends Event> T getLastEvent(Class<T> event) {
+        return this.xEventTransaction.getLatestEvent(event);
+    }
 
-	public <T extends Event> T getLatestEvent() {
-		return this.xEventTransaction.getLatestEvent();
-	}
+    public <T extends Event> T getLastEvent() {
+        return this.xEventTransaction.getLatestEvent();
+    }
 
-	public EventTransaction newChild(Event nextEvent, String nextWorkflow) {
-		return EventTransaction.builder()
-				.parentId(getOriginId())
-				.workflow(nextWorkflow)
-				.events(Lists.newArrayList(nextEvent))
-				.build();
-	}
+    public EventTransaction newChild(Event nextEvent, String nextWorkflow) {
+        return EventTransaction.builder()
+                .parentId(getOriginId())
+                .workflow(nextWorkflow)
+                .events(Lists.newArrayList(nextEvent))
+                .build();
+    }
 
-	public void store(Event event) {
-		requireNonNull(event, "Event cannot be null");
-		this.xEventTransaction.store(event);
-		event.nextUpdate(this.xEventTransaction);
-		var onNextState = event.nextState();
-		onNextState.ifPresent(this.xEventTransaction::triggerStateTransition);
-	}
-
+    public void store(Event event) {
+        requireNonNull(event, "Event cannot be null");
+        this.xEventTransaction.store(event);
+        event.nextUpdate(this.xEventTransaction);
+        var onNextState = event.nextState();
+        onNextState.ifPresent(this.xEventTransaction::triggerStateTransition);
+    }
 }
