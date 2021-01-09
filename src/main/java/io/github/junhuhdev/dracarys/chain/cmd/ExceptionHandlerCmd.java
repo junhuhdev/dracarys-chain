faea1f5bbc1846e4596da4e3e5d3623fffe23d04
Jun Huh <junhuhdev@gmail.com>
2021-01-09 16:38:04 +0100

    fix

--- src/main/java/io/github/junhuhdev/dracarys/chain/cmd/ExceptionHandlerCmd.java
+++ src/main/java/io/github/junhuhdev/dracarys/chain/cmd/ExceptionHandlerCmd.java
@@ -1,9 +1,9 @@
 package io.github.junhuhdev.dracarys.chain.cmd;
 
-import com.tele2.orderflow.mcls.mclsparent.chain.Chain;
-import com.tele2.orderflow.mcls.mclsparent.chain.ChainContext;
-import com.tele2.orderflow.mcls.mclsparent.event.FaultEvent;
-import com.tele2.orderflow.mcls.mclsparent.jdbc.EventJdbcRepository;
+import io.github.junhuhdev.dracarys.chain.chain.Chain;
+import io.github.junhuhdev.dracarys.chain.chain.ChainContext;
+import io.github.junhuhdev.dracarys.chain.event.FaultEvent;
+import io.github.junhuhdev.dracarys.chain.jdbc.EventJdbcRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -11,20 +11,19 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 @Component
 @Slf4j
-public class ExceptionHandlerCmd implements com.tele2.orderflow.mcls.mclsparent.command.Command {
+public class ExceptionHandlerCmd implements Command {
 
-	private final EventJdbcRepository eventJdbcRepository;
+    private final EventJdbcRepository eventJdbcRepository;
 
-	@Override
-	public ChainContext execute(ChainContext ctx, Chain chain) {
-		try {
-			return chain.proceed(ctx);
-		} catch (Exception e) {
-			log.error("Exception was thrown while processing event={}", ctx.getEventTransaction(), e);
-			ctx.store(new FaultEvent(e));
-			eventJdbcRepository.update(ctx.getEventTransaction());
-		}
-		return ctx;
-	}
-
+    @Override
+    public ChainContext execute(ChainContext ctx, Chain chain) {
+        try {
+            return chain.proceed(ctx);
+        } catch (Exception e) {
+            log.error("Exception was thrown while processing event={}", ctx.getEventTransaction(), e);
+            ctx.store(new FaultEvent(e));
+            eventJdbcRepository.update(ctx.getEventTransaction());
+        }
+        return ctx;
+    }
 }
--- src/main/java/io/github/junhuhdev/dracarys/chain/cmd/FinalizeCmd.java
+++ src/main/java/io/github/junhuhdev/dracarys/chain/cmd/FinalizeCmd.java
@@ -1,18 +1,17 @@
 package io.github.junhuhdev.dracarys.chain.cmd;
 
-import com.tele2.orderflow.mcls.mclsparent.chain.Chain;
-import com.tele2.orderflow.mcls.mclsparent.chain.ChainContext;
+import io.github.junhuhdev.dracarys.chain.chain.Chain;
+import io.github.junhuhdev.dracarys.chain.chain.ChainContext;
 import org.springframework.stereotype.Component;
 
 /**
  * Command that indicates end of chain process
  */
 @Component
-public class FinalizeCmd implements com.tele2.orderflow.mcls.mclsparent.command.Command {
+public class FinalizeCmd implements Command {
 
-	@Override
-	public ChainContext execute(ChainContext ctx, Chain chain) {
-		return ctx;
-	}
-
+    @Override
+    public ChainContext execute(ChainContext ctx, Chain chain) {
+        return ctx;
+    }
 }
